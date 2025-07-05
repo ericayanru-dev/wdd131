6c98cb2b8f7335d5eae1319c20ea6f91be2653b5
ericayanru-dev <ericayanru@outlook.com>
2025-07-05 18:32:14 +0100

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,5 @@
-const year = document.querySelector("#currentyear");
-const oLastModif = document.querySelector("#modification");
-
-const today = new Date();
-const modification = new Date(document.lastModified);
-
-year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
-oLastModif.innerHTML = `<span class="highlight">${modification.toLocaleString()}</span>`;
+const
+    year = document.querySelector("#currentyear"), oLastModif = document.querySelector("#modification"), today = new
+        Date, modification = new Date(document.lastModified); year.innerHTML = `<span 
+        class="highlight">${today.getFullYear()}</span>`, oLastModif.innerHTML = `<span 
+        class="highlight">${modification.toLocaleString()}</span>`;
